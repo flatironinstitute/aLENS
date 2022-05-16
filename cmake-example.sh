03baa2294fcdf76c7cb35e25c9173b06d1139bc1
Adam Lamson <alamson@flatironinstitute.org>
2022-05-16 18:01:29 -0400

    No longer shallow merge in CMakeLists

--- cmake-example.sh
+++ cmake-example.sh
@@ -2,22 +2,24 @@
 
 # remember to use Release and proper flags for production runs
 # examples for gcc:
-# for production run:
-# -D CMAKE_BUILD_TYPE=Release \
-# -D CMAKE_CXX_FLAGS="-O3 -march=native -DNDEBUG" \
-# for debug/development:
-# -D CMAKE_BUILD_TYPE=Debug \
-# -D CMAKE_CXX_FLAGS="-O0" \
-
-# choose a proper install destination
-# change SFTPATH to where all dependencies are installed
-
+# Production
 cmake \
   -D CMAKE_CXX_COMPILER=mpicxx \
   -D CMAKE_C_COMPILER=mpicc \
   -D CMAKE_BUILD_TYPE=Release \
-  -D ENABLE_TEST=ON \
-  -D CMAKE_CXX_FLAGS="-O3 -march=native -DNDEBUG" \
-  -D CMAKE_INSTALL_PREFIX="/your/installation/location" \
-  -D SFTPATH="/your/dependency/library/location" \
-  ../
+  -D CMAKE_CXX_FLAGS="-O3 -march=broadwell -DNDEBUG" \
+  -D CMAKE_INSTALL_PREFIX="${HOME}/Run/AMSOS" \
+  -D SFTPATH="${HOME}/local/" \
+  -D Eigen3_DIR="${SFTPATH}/share/eigen3/cmake" \
+../
+
+# cmake \
+#  -D CMAKE_CXX_COMPILER=mpicxx \
+#  -D CMAKE_C_COMPILER=mpicc \
+#  -D CMAKE_BUILD_TYPE=Debug \
+#  -D ENABLE_TEST=ON \
+#  -D CMAKE_CXX_FLAGS="-O0 -march=broadwell -DDEBUG" \
+#  -D CMAKE_INSTALL_PREFIX="${HOME}/Run/AMSOS" \
+#  -D SFTPATH="${HOME}/local/" \
+#  -D Eigen3_DIR="${SFTPATH}/share/eigen3/cmake" \
+# ../
