3b5e5fd0f0b6e8d76c090e48671662539d2f0427
Adam Lamson <alamson@flatironinstitute.org>
2022-06-01 11:46:42 -0400

    Make cmake-example.sh more general for instructive purposes.

--- cmake-example.sh
+++ cmake-example.sh
@@ -7,19 +7,18 @@ cmake \
   -D CMAKE_CXX_COMPILER=mpicxx \
   -D CMAKE_C_COMPILER=mpicc \
   -D CMAKE_BUILD_TYPE=Release \
-  -D CMAKE_CXX_FLAGS="-O3 -march=broadwell -DNDEBUG" \
-  -D CMAKE_INSTALL_PREFIX="${HOME}/Run/AMSOS" \
-  -D SFTPATH="${HOME}/local/" \
-  -D Eigen3_DIR="${SFTPATH}/share/eigen3/cmake" \
+  -D CMAKE_CXX_FLAGS="-O3 -march=native -DNDEBUG" \
+  -D CMAKE_INSTALL_PREFIX="/your/installation/location" \
+  -D SFTPATH="/your/dependency/library/location" \
 ../
 
+## Debug
 # cmake \
 #  -D CMAKE_CXX_COMPILER=mpicxx \
 #  -D CMAKE_C_COMPILER=mpicc \
 #  -D CMAKE_BUILD_TYPE=Debug \
 #  -D ENABLE_TEST=ON \
-#  -D CMAKE_CXX_FLAGS="-O0 -march=broadwell -DDEBUG" \
-#  -D CMAKE_INSTALL_PREFIX="${HOME}/Run/AMSOS" \
-#  -D SFTPATH="${HOME}/local/" \
-#  -D Eigen3_DIR="${SFTPATH}/share/eigen3/cmake" \
+#  -D CMAKE_CXX_FLAGS="-O0 -march=native -DDEBUG" \
+#  -D CMAKE_INSTALL_PREFIX="/your/installation/location" \
+#  -D SFTPATH="/your/dependency/library/location" \
 # ../
